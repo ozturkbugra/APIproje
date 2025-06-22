bc18bc18b978fdf372ec405ea795fc4565de7c86
ozturkbugra <bugra_ozturk1905@hotmail.com>
2025-06-22 14:25:34 +0300

    Giriş yapma kodu yapıldı daha sonra token bilgisi eklenecek

--- APIproje/Controllers/UsersController.cs
+++ APIproje/Controllers/UsersController.cs
@@ -11,10 +11,12 @@ namespace APIproje.Controllers
     public class UsersController : ControllerBase
     {
         private UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
 
-        public UsersController(UserManager<AppUser> userManager)
+        public UsersController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         [HttpPost("register")]
@@ -46,5 +48,26 @@ namespace APIproje.Controllers
 
 
         }
+
+        public async Task<IActionResult> Login(LoginDto model)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+
+            if(user == null)
+            {
+                return BadRequest(new {message = "Mail Bulunamadı"});
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password,false);
+
+            if (result.Succeeded)
+            {
+                return Ok(
+                    new { token = "token" }
+                );
+            }
+            return Unauthorized();
+        }
+ 
     }
 }
