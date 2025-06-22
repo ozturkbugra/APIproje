1585c121e7c71be47fb50788f16a30fdbb28044c
ozturkbugra <bugra_ozturk1905@hotmail.com>
2025-06-22 14:17:52 +0300

    Kullanıcı Ekleme işlemi yapıldı.

---
+++ APIproje/Controllers/UsersController.cs
@@ -0,0 +1,50 @@
+﻿using APIproje.Dto;
+using APIproje.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIproje.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private UserManager<AppUser> _userManager;
+
+        public UsersController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> CreateUser(UserDto model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+
+            }
+            var user = new AppUser
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+                FullName = model.FullName,
+                DateAdded = DateTime.Now
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                return StatusCode(201);
+            }
+
+
+
+            return BadRequest(result.Errors);
+
+
+        }
+    }
+}
